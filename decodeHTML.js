64d261a5dd9c4d1e04edeaecc56b668f30e4be33
Seren <SerenTera@users.noreply.github.com>
2017-08-21 19:41:59 +0800

    Added removal of HTML outer thingy

--- decodeHTML.js
+++ decodeHTML.js
@@ -11,6 +11,7 @@ const entities = {
 module.exports = {
 	decodeHTMLEntities(s) {
 		return (s
+			.replace(/<\/?[^<>]*>/gi, '')
 			.replace(/&#(\d+);?/g, (_, code) => String.fromCharCode(code))
 			.replace(/&#[xX]([A-Fa-f0-9]+);?/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)))
 			.replace(/&([^;\W]+;?)/g, (m, e) => entities[e.replace(/;$/, '')] || m)
